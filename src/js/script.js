45533027c7724f5bec2a0ea04fb315d25a535187
najibf911 <najib1802@gmail.com>
2025-09-02 13:27:25 +0700

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ const searchBar = document.getElementById("search-bar");
 const todoItems = document.getElementById("todo-items");
 
 function addTodo() {
- if (searchBar.value.trim() === '') {
+ if (searchBar.value === '') {
   alert("Mohon isikan tugas.");
  } else {
   let li = document.createElement("li");
