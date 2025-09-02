899a3209b6ca90549bf0af52570180a38c3dca88
najibf911 <najib1802@gmail.com>
2025-09-02 13:31:35 +0700

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,8 @@ const todoItems = document.getElementById("todo-items");
 function addTodo() {
  if (searchBar.value === "") {
   alert("Mohon isikan tugas.");
- } else {
+  return;
+ }
   let li = document.createElement("li");
   li.className = "flex justify-between items-center p-2 border-b";
 
